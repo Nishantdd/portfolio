faf3fb3c61e9513fd7897e50b0af8168d4984669
Nishantdd <nishant.dahiya2000@gmail.com>
2024-11-26 13:20:15 +0530

    batman commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    eslint: {
+        dirs: ['pages', 'components', 'lib', 'src'], // Directories to lint
+        ignoreDuringBuilds: false
+    }
 };
 
 export default nextConfig;
